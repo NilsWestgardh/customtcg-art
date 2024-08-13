8db9c9e99e5acdd9713b905d4c6ecda56e4fccda
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-08-12 22:43:01 -0700

    add ai disclosure

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 const disclaimers = [
+  "CustomTCG.art uses OpenAI's DALL-E 3 to generate art.",
   "CustomTCG.art is a free fan content service not affiliated with, approved by, endorsed by, or produced by Wizards of the Coast, Hasbro, Activision Blizzard, Nuverse, Second Dinner, Marvel, or Konami.",
   "Contact: admin@play.nexus",
 ]
